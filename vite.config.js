79d38619e3028b0fcd1601d4e6b81f3fa097164d
NoamFav <95364866+NoamFav@users.noreply.github.com>
2025-10-29 19:25:40 +0100

    refactor(main): Simplified Vite configuration by removing an unused constant and modifying the import statement for FORM_ENDPOINT variable.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export const FORM_ENDPOINT = import.meta.env.VITE_FORM_ENDPOINT;
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
